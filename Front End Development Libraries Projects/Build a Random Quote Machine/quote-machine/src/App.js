3db76a4495772145983d79c56e3125902532f30e
SandraVLP <aleksanvp@yandex.ru>
2024-01-22 17:23:19 +0300

    убрать ненужное

--- Front End Development Libraries Projects/Build a Random Quote Machine/quote-machine/src/App.js
+++ Front End Development Libraries Projects/Build a Random Quote Machine/quote-machine/src/App.js
@@ -1,13 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Button, Row } from "react-bootstrap";
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Container from 'react-bootstrap/Container';
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Container from "react-bootstrap/Container";
 import { Twitter } from "react-bootstrap-icons";
 import { useEffect, useState } from "react";
 
 function App() {
-const [quote, setQuote] = useState({});
+  const [quote, setQuote] = useState({});
 
   useEffect(() => {
     getQuote();
@@ -20,37 +19,36 @@ const [quote, setQuote] = useState({});
       })
       .then((data) => {
         setQuote({
-        author: data.author,
-        text: data.content,
-        tweetUrl: "https://twitter.com/intent/tweet/?text=" + data.content.replace(/ /g, "+")
-      })
+          author: data.author,
+          text: data.content,
+          tweetUrl:
+            "https://twitter.com/intent/tweet/?text=" +
+            data.content.replace(/ /g, "+"),
+        });
       })
       .catch((err) => {
         console.log(`Ошибка; ${err}`);
       });
   };
 
-
   return (
     <div className="App" id="app">
       <Container id="quote-box">
-        <Row id="quote-row" className="justify-content-md-center" >
-          <div id="text">
-            {quote.text}
-          </div>
-          <div id="author">  {quote.author}</div>
+        <Row id="quote-row" className="justify-content-md-center">
+          <div id="text">{quote.text}</div>
+          <div id="author"> {quote.author}</div>
         </Row>
         <Row lg="3" className="justify-content-md-center">
-        <ButtonGroup size="lg" className="mb-2">
-        <Button id="new-quote" variant="outline-primary" onClick={getQuote}>
-          Get new Quote
-        </Button>
+          <ButtonGroup size="lg" className="mb-2">
+            <Button id="new-quote" variant="outline-primary" onClick={getQuote}>
+              Get new Quote
+            </Button>
 
-       <Button href={quote.tweetUrl}  id="tweet-quote" variant="primary">
-          Tweet Quote
-        <Twitter color="white" size={30} />
-        </Button> 
-        </ButtonGroup>
+            <Button href={quote.tweetUrl} id="tweet-quote" variant="primary">
+              Tweet Quote
+              <Twitter id="tweet-icon" color="white" size={30} />
+            </Button>
+          </ButtonGroup>
         </Row>
       </Container>
     </div>
