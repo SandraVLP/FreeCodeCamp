1a7f7f4d89d25d13d275454b5700b1b4436b398a
SandraVLP <aleksanvp@yandex.ru>
2024-01-23 20:05:13 +0300

    текст отображается

--- Front End Development Libraries Projects/markdown-previewer/src/App.js
+++ Front End Development Libraries Projects/markdown-previewer/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [text, setText] = useState('');
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
   return (
     <div className="App">
- <textarea id="editor"></textarea>
- <div id="preview"></div>
+ <textarea id="editor" onChange={handleChange}></textarea>
+ <div id="preview">{text}</div>
     </div>
   );
 }
