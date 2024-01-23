3e13a4d77954b688587db0f67903f7d8cf0b5fcf
SandraVLP <aleksanvp@yandex.ru>
2024-01-23 20:35:51 +0300

    marked works

--- Front End Development Libraries Projects/markdown-previewer/src/App.js
+++ Front End Development Libraries Projects/markdown-previewer/src/App.js
@@ -1,22 +1,28 @@
-
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
 function App() {
-
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   function handleChange(e) {
     setText(e.target.value);
   }
+
   return (
     <div className="App">
-      <div className='container'>
- <textarea id="editor" onChange={handleChange} placeholder='Text' value={text}></textarea>
- <div id="preview">{marked.parse(text)}</div>
-
- </div>
+      <div className="container">
+        <textarea
+          id="editor"
+          onChange={handleChange}
+          placeholder="Text"
+          value={text}
+        ></textarea>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(text) }}
+        ></div>
+      </div>
     </div>
   );
 }
