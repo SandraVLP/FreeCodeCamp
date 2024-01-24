efef0b97d7448913d17daebba38954b370c851e7
SandraVLP <aleksanvp@yandex.ru>
2024-01-24 16:27:13 +0300

    class basics

--- Front End Development Libraries Projects/JS calc/index.js
+++ Front End Development Libraries Projects/JS calc/index.js
@@ -4,3 +4,32 @@ const clear = document.getElementById('clear');
 const equals = document.getElementById('equals');
 const output = document.getElementById('display');
 const result = document.querySelectorAll('formulaScreen');
+
+
+class Calculator {
+    constructor(output, result) {
+        this.output = output
+        this.result = result
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(opertion) {
+
+    }
+
+    count() {
+
+    }
+
+    clear() {
+
+    }
+
+    updateScreen () {
+        
+    }
+}
