7295f22f754b95056d154455fb853bcb7622da68
SandraVLP <aleksanvp@yandex.ru>
2024-01-24 16:15:20 +0300

    js const

---
+++ Front End Development Libraries Projects/JS calc/index.js
@@ -0,0 +1,6 @@
+const number = document.querySelectorAll('btn-number');
+const operation = document.querySelectorAll('btn-operation');
+const clear = document.getElementById('clear');
+const equals = document.getElementById('equals');
+const output = document.getElementById('display');
+const result = document.querySelectorAll('formulaScreen');
